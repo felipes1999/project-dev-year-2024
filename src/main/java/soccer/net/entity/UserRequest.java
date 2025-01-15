2ed8da311fa7a7947197e0ef52f14d9e778fc27d
Felipe Silva <az3578744@gmail.com>
2025-01-14 21:33:00 -0300

    correção de error no banco de dados

--- src/main/java/soccer/net/entity/UserRequest.java
+++ src/main/java/soccer/net/entity/UserRequest.java
@@ -1,12 +1,10 @@
 package soccer.net.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.util.Objects;
 
+@Entity
 public class UserRequest {
 
     @Override
